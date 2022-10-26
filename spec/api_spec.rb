4ccd30e55783369a1e055bd42fd9b6d474a838b0
Soumya Ray <soumya.ray@gmail.com>
2022-10-26 19:18:39 +0800

    Test setup moved to spec_helper

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'spec_helper'
+
 describe 'these are my tests' do
   # tests go here
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# LOAD CONSTANTS
+# SETUP VCR
