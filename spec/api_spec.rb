894e532ffc9f685e27d70c06f22ade57ad7bfbda
emily <emilyvicki601@gmail.com>
2022-10-26 19:51:06 +0800

    Lets make a conflict

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,8 +2,13 @@
 
 require_relative 'spec_helper'
 
-describe 'these are my tests' do
+describe 'these are Emilys tests' do
   it 'should work all the time!' do
     # my amazing tests are here
+    # mine is better
+  end
+
+  it 'should worked all the time!' do
+    # mine is much better
   end
 end
